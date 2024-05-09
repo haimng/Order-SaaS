5af928be8b3c85ca8ee86f612eeb192239d1b18d
Hai M Nguyen <vnjp22@gmail.com>
2024-05-09 11:04:28 -0700

    fixed unescaped quote

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">          
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Orders SaaS: </strong> 
-            Your tailored solution for small retailers' order management needs. Simplify order processing, tracking, inventory management, and fulfillment. Ideal for small-scale retail businesses looking to streamline operations.
+            Your tailored solution for small retailers order management needs. Simplify order processing, tracking, inventory management, and fulfillment. Ideal for small-scale retail businesses looking to streamline operations.
           </p>          
         </div>        
       </div>
